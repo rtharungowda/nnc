7740a8b1275ff9c210edf8ad3198b5ae2db3f5a7
rtharungowda <rtharungowda.cd.ece19@itbhu.ac.in>
2021-02-23 10:14:08 +0000

    Add : yaml files for kfold

--- perform.py
+++ perform.py
@@ -7,10 +7,14 @@ parser.add_argument("-m","--mode",
                     help="enter train for training and predict for prediction",
                     default="train",
                     type=str)
-parser.add_argument("-l","--link",
-                    help="link to yaml file while mode is training",
+parser.add_argument("-v","--val",
+                    help="type of validation used for training, kcv_<num> - k-fold cross validation and sv = simple validation",
                     type=str,
-                    default="/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/diatom.yaml")
+                    default="kcv")
+parser.add_argument("-l","--link",
+                    help="link to folder containing yaml file for training",
+                    type=str,
+                    default="/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/")
 parser.add_argument("-s","--source",
                     help="link to source of images for prediction",
                     type=str,
@@ -41,8 +45,21 @@ parser.add_argument("-c","--conf",
                     type=float)
 args = parser.parse_args()
 if args.mode == "train":
-    print(f"training yolo model <{args.type}> on {args.link} yaml file, of batch size {args.batch} and for {args.epochs} epochs on device {args.device}")
-    os.system(f'python3 /content/yolov5/train.py --batch {args.batch} --epochs {args.epochs} --data {args.link} --cfg /content/yolov5/models/yolov5{args.type}.yaml  --weights /content/yolov5/yolov5{args.type}.pt --device {args.device}')
+    print(f"training yolo model <{args.type}> on {args.link} yaml file,\n of batch size {args.batch} and for {args.epochs} epochs on device {args.device}")
+    if args.val == sv:
+        args.link +="diatom.yaml"
+        os.system(f'python3 /content/yolov5/train.py --batch {args.batch} --epochs {args.epochs} --data {args.link} --cfg /content/yolov5/models/yolov5{args.type}.yaml  --weights /content/yolov5/yolov5{args.type}.pt --device {args.device}')
+    else :
+        yaml_file = args.link+"_0.yaml"
+        os.system(f'python3 /content/yolov5/train.py --batch {args.batch} --epochs {args.epochs} --data {yaml_file} --cfg /content/yolov5/models/yolov5{args.type}.yaml  --weights /content/yolov5/yolov5{args.type}.pt --device {args.device}')
+        k = int(args.val.split('_')[1])
+        for i in range(1,k):
+            if i!=1:
+                new_weight = f"/content/yolov5/runs/train/exp{str(i+1)}/weights/best.pt"
+            else :
+                new_weight = f""
+            yaml_file = args.link+"_"+str(i)+".yaml"
+            os.system(f'python3 /content/yolov5/train.py --batch {args.batch} --epochs {args.epochs} --data {yaml_file} --cfg /content/yolov5/models/yolov5{args.type}.yaml  --weights /content/yolov5/yolov5{args.type}.pt --device {args.device}')
 else :
     print(f"predicting on images in folder {args.source} using weights {args.weights} with confidance score {args.conf}")
     os.system(f'python3 detect.py --source {args.source} --weights {args.weights} --conf {args.conf}')
