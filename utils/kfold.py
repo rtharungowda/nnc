9c9f592ae50fcc73b66a626eb40f040ff092da00
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-02-24 07:24:28 +0000

    Call function if main only

--- utils/kfold.py
+++ utils/kfold.py
@@ -2,37 +2,43 @@ import glob
 import os
 from sklearn.model_selection import KFold
 
-#find all images in directory
-imgs_list = glob.glob("/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/imgs/*.jpg")
-print(f"Total number of images {len(imgs_list)}")
+def create_folds(img_path,txt_path,folds=5):
+    #find all images in directory
+    imgs_list = glob.glob(img_path+"*.jpg")
+    print(f"Total number of images {len(imgs_list)}")
 
-#kfold obj
-kf =KFold(n_splits=5, shuffle=True, random_state=42)
+    #kfold obj
+    kf =KFold(n_splits=folds, shuffle=True, random_state=42)
 
-for k,(train_ind, test_ind) in enumerate(kf.split(imgs_list)):
-    print(f"fold {k} number of training images {len(train_ind)} and number of validation images {len(test_ind)}")
+    for k,(train_ind, test_ind) in enumerate(kf.split(imgs_list)):
+        print(f"fold {k} number of training images {len(train_ind)} and number of validation images {len(test_ind)}")
 
-    #trainging .txt file
-    pth = f"/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/train_yolo_{str(k)}.txt"
-    if os.path.exists(pth):
-        os.remove(pth)
-    for i in train_ind:
-        with open(pth, "a+") as file_object:
-            file_object.seek(0)
-            data = file_object.read(100)
-            if len(data) > 0 :
-                file_object.write("\n")
-            file_object.write(imgs_list[i])
+        #trainging .txt file
+        pth = txt_path+f"train_yolo_{str(k)}.txt"
+        if os.path.exists(pth):
+            os.remove(pth)
+        for i in train_ind:
+            with open(pth, "a+") as file_object:
+                file_object.seek(0)
+                data = file_object.read(100)
+                if len(data) > 0 :
+                    file_object.write("\n")
+                file_object.write(imgs_list[i])
+        
+        #validation .txt file
+        pth = txt_path+f"val_yolo_{str(k)}.txt"
+        if os.path.exists(pth):
+            os.remove(pth)
+        for i in test_ind:
+            with open(pth, "a+") as file_object:
+                file_object.seek(0)
+                data = file_object.read(100)
+                if len(data) > 0 :
+                    file_object.write("\n")
+                file_object.write(imgs_list[i])
     
-    #validation .txt file
-    pth = f"/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/val_yolo_{str(k)}.txt"
-    if os.path.exists(pth):
-        os.remove(pth)
-    for i in test_ind:
-        with open(pth, "a+") as file_object:
-            file_object.seek(0)
-            data = file_object.read(100)
-            if len(data) > 0 :
-                file_object.write("\n")
-            file_object.write(imgs_list[i])
-    
+if __name__ == "__main__":
+    img_path = "/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/imgs/"
+    txt_path = "/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/"
+    folds = 5
+    create_folds(img_path,txt_path,folds)
--- utils/train_test_split.py
+++ utils/train_test_split.py
@@ -3,33 +3,42 @@ import os
 
 from sklearn.model_selection import train_test_split
 
-#find all images in directory
-imgs_list = glob.glob("/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/imgs/*.jpg")
-print(f"Total number of images {len(imgs_list)}")
 
-#make split
-train, test = train_test_split(imgs_list,test_size=0.2, random_state=42, shuffle=True)
-print(f"Number of training images = {len(train)} and validation images = {len(test)}")
+def create_split(img_path,txt_path,ratio=0.2):
+  #find all images in directory
+  imgs_list = glob.glob(img_path+"*.jpg")
+  print(f"Total number of images {len(imgs_list)}")
 
-#write to files
-pth = "/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/train_yolo.txt"
-if os.path.exists(pth):
+  #make split
+  train, test = train_test_split(imgs_list,test_size=ratio, random_state=42, shuffle=True)
+  print(f"Number of training images = {len(train)} and validation images = {len(test)}")
+
+  #write to files
+  pth = txt_path+"train_yolo.txt"
+  if os.path.exists(pth):
+      os.remove(pth)
+  for i in train:
+    with open(pth, "a+") as file_object:
+      file_object.seek(0)
+      data = file_object.read(100)
+      if len(data) > 0 :
+          file_object.write("\n")
+      file_object.write(i)
+    
+  pth = txt_path+"val_yolo.txt"
+  if os.path.exists(pth):
     os.remove(pth)
-for i in train:
-  with open(pth, "a+") as file_object:
-    file_object.seek(0)
-    data = file_object.read(100)
-    if len(data) > 0 :
-        file_object.write("\n")
-    file_object.write(i)
-  
-pth = "/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/val_yolo.txt"
-if os.path.exists(pth):
-  os.remove(pth)
-for i in test:
-  with open(pth, "a+") as file_object:
-    file_object.seek(0)
-    data = file_object.read(100)
-    if len(data) > 0 :
-        file_object.write("\n")
-    file_object.write(i)
+  for i in test:
+    with open(pth, "a+") as file_object:
+      file_object.seek(0)
+      data = file_object.read(100)
+      if len(data) > 0 :
+          file_object.write("\n")
+      file_object.write(i)
+
+
+if __name__ == "__main__":
+  img_path = "/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/imgs/"
+  txt_path = "/content/Diatom-Non-neuronal-Cognition/Dataset/30_bbg/related_files/"
+  ratio = 0.2
+  create_split(img_path,txt_path,ratio)
